03ecf9e1159528ef302dc28e1eed5183ed3e2c22
elquimiquito <whiteheaded12@gmail.com>
2022-12-08 00:21:11 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+    public Error() {
+        active = false;
+        status = 250;
+        message = "Ошибка в 34-ой строке";
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Результат: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error task1 = new Error();
+        task1.printInfo();
+        Error task2 = new Error(true, 125, "Ошибок нет");
+        task2.printInfo();
+        Error task3 = new Error(false,0,"Все очень плохо");
+        task3.printInfo();
+    }
+}
