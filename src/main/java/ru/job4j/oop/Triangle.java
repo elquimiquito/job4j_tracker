be6f10b8914a4224827378d9649be3fd88b33733
elquimiquito <whiteheaded12@gmail.com>
2022-12-08 23:04:59 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,37 @@
+package ru.job4j.oop;
+
+import static java.lang.Math.sqrt;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double semiPerimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        return (ab + bc > ac) && (ac + ab > bc) && (bc + ac > ab);
+    }
+
+    public double area() {
+        double rsl = 1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        if (this.exist(ab, ac, bc)) {
+            double p = semiPerimeter(ab, ac, bc);
+            rsl = sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        } else {
+            return -1;
+        }
+        return rsl;
+    }
+}
