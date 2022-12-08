8c5214bdc8d36a30559793cce1d6084abdc5dca1
elquimiquito <whiteheaded12@gmail.com>
2022-12-08 17:49:23 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,38 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public static int minus(int a){
+        return a - x;
+    }
+
+    public int divide(int a) {
+        return a / x;
+    }
+
+    public int sumAllOperations(int a) {
+        return sum(a) + multiply(a) + minus(a) + divide(a);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int rsl = calculator.multiply(5);
+        System.out.println(rsl);
+        rsl = calculator.divide(5);
+        System.out.println(rsl);
+        rsl = minus(5);
+        System.out.println(rsl);
+        rsl = calculator.sumAllOperations(5);
+        System.out.println(rsl);
+    }
+}
