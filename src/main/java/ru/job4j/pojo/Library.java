b1cb3fbe63a6fd7a1e6f8300ebab85cf95e1f21f
elquimiquito <whiteheaded12@gmail.com>
2022-12-12 00:38:20 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book first = new Book("Властелин колец", 1000);
+        Book second = new Book("Унесенные ветром", 400);
+        Book third = new Book("Война и мир", 1250);
+        Book fourth = new Book("Clean code", 240);
+        Book[] books = new Book[4];
+        books[0] = first;
+        books[1] = second;
+        books[2] = third;
+        books[3] = fourth;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + " - " + bk.getNumberOfPages());
+        }
+        Book temporal = books[3];
+        books[3] = books[0];
+        books[0] = temporal;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + " - " + bk.getNumberOfPages());
+        }
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getName().equals("Clean code")) {
+                System.out.println(books[i].getName());
+            }
+        }
+    }
+}
