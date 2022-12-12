2ce9ebc41816579a5037a8774b1e1715ba96f1e0
elquimiquito <whiteheaded12@gmail.com>
2022-12-12 19:27:52 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -6,8 +6,8 @@ public class JSONReport extends TextReport {
     public String generate(String name, String body) {
         String ln = System.lineSeparator();
         return "{" + ln
-                + "\t\"name\" : " + "\"" + name + "\"," + ln
-                + "\t\"body\" : " + "\"" + body + "\"" + ln
+                + "\t\"name\" : \"" + name + "\"," + ln
+                + "\t\"body\" : \"" + body + "\"" + ln
                 + "}";
     }
 }
