9a079904ff2937ddd9984af5ccb8aaeb3a76ce97
elquimiquito <whiteheaded12@gmail.com>
2022-12-12 19:23:52 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,14 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        LocalDateTime dateTime = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-EEEE-yyyy HH:mm:ss");
-        String dateTimeFormat =  dateTime.format(formatter);
-        System.out.println(dateTimeFormat);
+        Item item = new Item(15, "Igor");
+        System.out.println(item);
     }
 }
