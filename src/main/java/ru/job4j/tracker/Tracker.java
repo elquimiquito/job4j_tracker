e9371df3361383aa284ada3470c6f5f2430ec446
elquimiquito <whiteheaded12@gmail.com>
2022-12-16 00:55:10 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #370444]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,7 +59,6 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            items[index] = null;
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
