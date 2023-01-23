acab79d5462176af0ab70460456d9a6a31594ba5
elquimiquito <whiteheaded12@gmail.com>
2023-01-23 15:36:11 +0300

    Дозагрузка

---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CountTest {
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Count.add(10, 2);
+                });
+        assertThat(exception.getMessage()).isEqualTo("Start should be less than finish.");
+    }
+
+    @Test
+    public void whenNoException() {
+        int start = 0;
+        int finish = 3;
+        int expected = 3;
+        int result = Count.add(start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
+}
