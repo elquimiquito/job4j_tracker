1478fada19e2f321216ec53cb87a8b05e5ad4398
elquimiquito <whiteheaded12@gmail.com>
2023-01-23 15:59:38 +0300

    п

--- src/test/java/ru/job4j/ex/CountTest.java
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -6,15 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CountTest {
-    @Test
-    public void whenException() {
-        IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class,
-                () -> {
-                    Count.add(10, 2);
-                });
-        assertThat(exception.getMessage()).isEqualTo("Start should be less than finish.");
-    }
 
     @Test
     public void whenNoException() {
