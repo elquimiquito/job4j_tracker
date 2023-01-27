a81a7ebd1f49e76b5a99183d8e866bc9e23b7701
elquimiquito <whiteheaded12@gmail.com>
2023-01-28 00:16:30 +0300

    0. Map, HashMap [#212652 #385213]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> people = new HashMap<>();
         people.put("whiteheaded12@gmail.com", "Bielogolovski I.I.");
+        people.put("whiteheaded12@gmail.com", "Ivanov I.I.");
+        people.put("vasyapupkin@mail.ru", "Pupkin V.V.");
         for (String key : people.keySet()) {
             String rsl = people.get(key);
             System.out.println(key + " = " + rsl);
