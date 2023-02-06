3610bc35c51c502c86d83f4c8860e5760388217c
elquimiquito <whiteheaded12@gmail.com>
2023-02-06 21:10:55 +0300

    6. Подсчет функции в диапазоне. [#24251]

---
+++ src/main/java/ru/job4j/function/FunctionCalculator.java
@@ -0,0 +1,15 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionCalculator {
+    public List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> rsl = new ArrayList<>();
+        for (double i = start; i < end; i++) {
+            rsl.add(func.apply(i));
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/function/FunctionCalculatorTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.function;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FunctionCalculatorTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(2, 5, x -> Math.pow(x, 2) + 3);
+        List<Double> expected = Arrays.asList(7D, 12D, 19D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    public void whenExponentialFunctionThenExponentialResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(2, 6, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(8D, 16D, 32D);
+        assertThat(result).containsAll(expected);
+    }
+}
